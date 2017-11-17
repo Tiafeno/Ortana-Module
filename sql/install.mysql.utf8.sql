1464344c6823f476c53a4537c35d8e406b2a0f73
Tiafeno <tiafenofnel@gmail.com>
2017-11-17 18:03:00 +0300

    change ortana_clients to ortana_articles

--- sql/install.mysql.utf8.sql
+++ sql/install.mysql.utf8.sql
@@ -1,10 +1,8 @@
-CREATE TABLE IF NOT EXISTS `ortana_clients` (
-	`id` int(10) NOT NULL AUTO_INCREMENT,
-	`client` text NOT NULL,
-	`contact` varchar(25) NOT NULL,
+CREATE TABLE IF NOT EXISTS `#__ortana_articles` (
+	`id` BIGINT(20) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	`title` TEXT NOT NULL,
+	`cost` INT NOT NULL,
+	`description` LONGTEXT NULL,
 
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
-
-INSERT INTO `ortana_clients` (`client`, `contact`) VALUES ('Hello World', 'xml@ortana.mg');
-INSERT INTO `ortana_clients` (`client`, `contact`) VALUES ('Hola Mundo', 'example@ortana.mg');
